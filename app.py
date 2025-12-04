b1401686eff7c6e63265c37c71cfe06a312fb427
JhonGM <jmgsoluciones@gmail.com>
2025-12-04 09:42:24 -0500

    s

--- app.py
+++ app.py
@@ -216,4 +216,6 @@ def ver_establecimientos():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
